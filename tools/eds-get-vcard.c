084284a9e03027e3d5e542db0703cad706a5e6c5
Ross Burton <ross@burtonini.com>
2008-02-11 17:00:17 +0000

    Add --uri support to eds-get-vcard

--- tools/eds-get-vcard.c
+++ tools/eds-get-vcard.c
@@ -1,21 +1,41 @@
 #include <libebook/e-book.h>
 #include "util.h"
 
+static char *uri = NULL;
+
+static const GOptionEntry options[] =  {
+  { "uri", 'u', 0, G_OPTION_ARG_STRING, &uri, "URI of book to open (default: system addressbook)." },
+  { NULL }
+};
+
+
 int
 main (int argc, char **argv)
 {
   GError *error = NULL;
+  GOptionContext *context;
   EBook *book;
   EContact *contact = NULL;
 
   g_type_init ();
 
+  context = g_option_context_new ("<UID> - get vCard");
+  g_option_context_add_main_entries (context, options, NULL);
+  if (!g_option_context_parse (context, &argc, &argv, &error)) {
+    g_error ("Cannot parse arguments: %s", error->message);
+  }
+
   if (argc != 2) {
-    g_print ("Usage: eds-get-vcard <UID>");
+    g_print ("Usage: eds-get-vcard <UID>\n");
     return 1;
   }
 
-  book = e_book_new_system_addressbook (&error);
+  if (uri) {
+    book = e_book_new_from_uri (uri, &error);
+    g_free (uri);
+  } else {
+    book = e_book_new_system_addressbook (&error);
+  }
   if (!book)
     die ("Cannot get book", error);
   
